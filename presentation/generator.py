fdc23ab73ab9dd464cac474290c051449472d6c7
aflaag <alessio.bandiera02@gmail.com>
2024-10-15 17:18:18 +0200

    pretty much nothing

--- presentation/generator.py
+++ presentation/generator.py
@@ -8,6 +8,9 @@ formulas = [
     r"\forall g, g' \in M \quad \Gamma(g) \cap \Gamma(g') = \varnothing",
     r"\omega(M) := \sum_{g \in M} |\Gamma(g)| - |\Gamma(M)|",
     r"W(M) := |\Gamma(M)| - \omega(M)",
+    r"W(M) := |\Gamma(M)| - 0",
+    r"\begin{array}{l}\mathbf{Definizione.} \ \mathrm{Data \ una \ matrice \ di \ mutazione} \ A \ \mathrm{di \ dimensioni} \ m \times n, \\ \mathrm{ed \ un \ intero} \ k > 0, \ \mathrm{si \ trovi \ una \ sottomatrice} \ m \times k \\ \mathrm{di} \ A \ \mathrm{tale \ da \ massimizzare} \ W(M).\end{array}",
+
 ]
 
 for i in range(len(formulas)):
